5997f90e406cdc450a8302daca65050250cbb761
Martin Tománek <tomanma9@cvut.cz>
2024-11-25 00:33:54 +0100

    Small progress

--- src/cooling/cooling.h
+++ src/cooling/cooling.h
@@ -1,28 +1,43 @@
 #pragma once
-
 /**
  * Generic simulated cooling solver
  * Requires a Problem, Configuration and Optimization Criterion
+ * If a class extends the provided base classes, then it is suitable for use
  *
  * Problem
- *  - can produce nextRandomConfiguration
- *  - can apply the configuration
- *  - can calculate the optimization criterion of a configuration
+ *  - can produce nextRandomConfiguration()
+ *  - can applyConfiguration(Configuration)
  * Configuration
  *  - the output we are looking for, based on which criterion is calculated
- * Optimization criterion // TODO: Maybe could do with just a number?
- *  - can compare itself to other criteria given a configuration and Problem
+ *  - Just passed around
+ * Optimization criterion
+ *  ? Maybe could do with just a number?
+ *  - can compare itself along the lines of {better|worse}[orEqual]()
  */
-class Criterion {};
-
-// Abstract base class for all configurations
+class Criterion {
+ public:
+  virtual bool operator==(const Criterion& other);
+  virtual bool operator<(const Criterion& other);
+  virtual bool operator>(const Criterion& other);
+  virtual bool operator>=(const Criterion& other);
+  virtual bool operator<=(const Criterion& other);
+};
 class Configuration {};
-
 class Problem {
   virtual Configuration nextRandomConfiguration();
   virtual void doConfiguration(const Configuration& configuration);
   virtual Criterion evaluateConfiguration(const Configuration& configuration);
 };
 
-template <typename Problem, typename Criterion>
-struct Cooling<Problem, Configuration, Criterion> {};
+
+
+class Cooling {
+ private:
+  Problem problem;
+  Configuration bestConfiguration;
+  Configuration current;
+  Criterion bestCriterion;
+
+ public:
+  Cooling(Problem problem, uint32_t maxTries, double coolingFactor, double stopTemperature, double startTemperature);
+};
