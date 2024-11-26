1cba89bf866a769b9ba063eb3515fb856f0b3297
Martin Tománek <tomanma9@cvut.cz>
2024-11-26 14:49:15 +0100

    Define interface

--- src/cooling/cooling.h
+++ src/cooling/cooling.h
@@ -1,43 +1,64 @@
 #pragma once
+#include <sat.h>
+
 /**
  * Generic simulated cooling solver
- * Requires a Problem, Configuration and Optimization Criterion
- * If a class extends the provided base classes, then it is suitable for use
+ * Requires a Problem, Configuration and Optimization Criteria
  *
  * Problem
- *  - can produce nextRandomConfiguration()
- *  - can applyConfiguration(Configuration)
+ * - Interface with following methods:
+ *  - Configuration getRandomNeighbor()
+ *  - Criteria evaluateConfiguration(Configuration)
+ *  - void applyConfiguration(Configuration)
  * Configuration
- *  - the output we are looking for, based on which criterion is calculated
- *  - Just passed around
- * Optimization criterion
- *  ? Maybe could do with just a number?
- *  - can compare itself along the lines of {better|worse}[orEqual]()
+ *  - Just output class we know nothing about and is passed around
+ * Optimization Criteria
+ *  - bool {better|worse}[orEqual](Criteria)
+ *  - double howMuch{better|worse}(Criteria)
  */
-class Criterion {
+class Criteria {
  public:
-  virtual bool operator==(const Criterion& other);
-  virtual bool operator<(const Criterion& other);
-  virtual bool operator>(const Criterion& other);
-  virtual bool operator>=(const Criterion& other);
-  virtual bool operator<=(const Criterion& other);
+  virtual bool operator==(const Criteria& other);
+  virtual bool operator<(const Criteria& other);
+  virtual bool operator>(const Criteria& other);
+  virtual bool operator>=(const Criteria& other);
+  virtual bool operator<=(const Criteria& other);
+  virtual double howMuchBetterThan(const Criteria& other);
+  virtual double howMuchWorseThan(const Criteria& other);
 };
 class Configuration {};
 class Problem {
-  virtual Configuration nextRandomConfiguration();
-  virtual void doConfiguration(const Configuration& configuration);
-  virtual Criterion evaluateConfiguration(const Configuration& configuration);
+  virtual Configuration getRandomNeighbor();
+  virtual Criteria evaluateConfiguration(const Configuration& configuration);
+  virtual void applyConfiguration(const Configuration& configuration);
 };
 
+template <typename T>
+concept Criteriable = requires(T t) {
+  { t == t } -> std::same_as<bool>;
+};
 
-
-class Cooling {
- private:
+template <typename Problem, typename Configuration, typename Criteria>
+class Cooling<Problem, Configuration, Criteria> {
   Problem problem;
   Configuration bestConfiguration;
   Configuration current;
-  Criterion bestCriterion;
+  Criteria bestCriteria;
 
  public:
-  Cooling(Problem problem, uint32_t maxTries, double coolingFactor, double stopTemperature, double startTemperature);
+  Cooling(
+      Problem problem, uint32_t maxTries, double coolingFactor,
+      double stopTemperature, double startTemperature
+  );
 };
+
+// =============================================================================
+/**
+ * Different approach to generality
+ *
+ * The Cooling will be interacting with class a class implementing Problem
+ * The Problem can be an intermediary class between the SAT and Cooling
+ * It will also provide the Criteria value class and calculation
+ * It will return the Configurations too
+ *
+ */
